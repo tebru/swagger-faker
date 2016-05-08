926b8b9a1e1360922f3ebc1607434fe03c3203c7
Nate Brunette <n@tebru.net>
2016-05-07 19:56:24 -0500

    Adding support for variable urls
    Signed-off-by: Nate Brunette <n@tebru.net>

--- src/Provider/SchemaProvider.php
+++ src/Provider/SchemaProvider.php
@@ -9,6 +9,7 @@ namespace Tebru\SwaggerFaker\Provider;
 use InvalidArgumentException;
 use League\JsonGuard\Dereferencer;
 use League\JsonGuard\Reference;
+use League\JsonGuard\Validator;
 use stdClass;
 
 /**
@@ -54,7 +55,7 @@ class SchemaProvider
         $basePath = $this->getProperty($swagger, 'basePath', '');
         $path = str_replace($basePath, '', $path);
         $paths = $this->getProperty($swagger, 'paths');
-        $pathObject = $this->getProperty($paths, $path);
+        $pathObject = $this->findInPaths($paths, $path, $operation);
         $operationObject = $this->getProperty($pathObject, $operation);
         $operationResponses = $this->getProperty($operationObject, 'responses');
 
@@ -111,5 +112,105 @@ class SchemaProvider
     public function hasProperty(stdClass $object, $property)
     {
         return property_exists($object, $property);
+    }
+
+    /**
+     * @param stdClass $paths
+     * @param string $path
+     * @param string $operation
+     * @return stdClass
+     * @throws InvalidArgumentException
+     */
+    private function findInPaths(stdClass $paths, $path, $operation)
+    {
+        $pathsArray = get_object_vars($paths);
+
+        if (array_key_exists($path, $pathsArray)) {
+            return $pathsArray[$path];
+        }
+
+        $pathParts = explode('/', $path);
+
+        $foundPath = null;
+        foreach ($pathsArray as $pathKey => $value) {
+            // assume we found it
+            $foundPath = $value;
+
+            if (false === strpos($pathKey, '{')) {
+                $foundPath = null;
+                continue;
+            }
+
+            // skip if we don't have the right operation
+            if (!$this->hasProperty($value, $operation)) {
+                $foundPath = null;
+                continue;
+            }
+
+            $operationProperty = $this->getProperty($value, $operation);
+
+            // skip if there aren't any parameters
+            if (!$this->hasProperty($operationProperty, 'parameters')) {
+                $foundPath = null;
+                continue;
+            }
+
+            $parameters = $this->getProperty($operationProperty, 'parameters');
+
+            $pathKeyParts = explode('/', $pathKey);
+
+            foreach ($pathKeyParts as $index => $part) {
+                // if the key we're checking doesn't exist, exit
+                if (!array_key_exists($index, $pathParts)) {
+                    $foundPath = null;
+                    break;
+                }
+
+                if (0 === strpos($part, '{')) {
+                    $name = substr($part, 1, -1);
+
+                    foreach ($parameters as $parameter) {
+                        $inProperty = $this->getProperty($parameter, 'in', '');
+                        if ($inProperty !== 'path') {
+                            $foundPath = null;
+                            continue;
+                        }
+
+                        $nameProperty = $this->getProperty($parameter, 'name', '');
+                        if ($name !== $nameProperty) {
+                            $foundPath = null;
+                            continue;
+                        }
+
+                        // required is not valid in v4 of json schema
+                        if ($this->hasProperty($parameter, 'required')) {
+                            unset($parameter->required);
+                        }
+
+                        $validator = new Validator($pathParts[$index], $parameter);
+                        if ($validator->fails()) {
+                            $foundPath = null;
+                            break;
+                        }
+                    }
+                } else {
+                    // if they're not equal, exit
+                    if ($part !== $pathParts[$index]) {
+                        $foundPath = null;
+                        break;
+                    }
+                }
+            }
+
+            if (null !== $foundPath) {
+                break;
+            }
+        }
+
+        if (null === $foundPath) {
+            throw new InvalidArgumentException('Could not find swagger path');
+        }
+
+        return $foundPath;
     }
 }
